69e0f1b64926abe0bee8d26f3b014037a985132f
Luan E <luan.erazo@skyrocketlab.space>
2021-09-09 09:30:39 -0500

    cambio carpeta logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="bg-black p-5">
   <div class="flex flex-col my-auto w-full h-full">
-<img class="max-h-full lg:max-h-96" width="100%" height="100%" src="~/assets/svg/Logo.svg" alt="Logo SkyRocket">
+<img class="max-h-full lg:max-h-96" width="100%" height="100%" src="~/assets/img/Logo.svg" alt="Logo SkyRocket">
 <h3 class="font-sans-space text-white text-5xl text-center">STUDIO</h3>
 
   </div>
